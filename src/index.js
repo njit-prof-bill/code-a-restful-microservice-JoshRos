3856d7ca9fb047d7288cacc8423b0347fb329e48
JoshRos <40119045+JoshRos@users.noreply.github.com>
2024-10-19 19:09:50 -0400

    Completed REST API microservice

--- src/index.js
+++ src/index.js
@@ -9,9 +9,63 @@ app.use(express.json());
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+let users=[];
+let nextId=1;
+
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+    const newUser = { id: nextId++, name, email };
+    users.push(newUser);
+    res.status(201).json(newUser);
 });
+
+app.get('/users/:id', (req, res) => {
+    const {id}=req.params;
+    const user=users.find(u =>u.id==id);
+    if(user)
+    {
+        res.json(user);
+    }
+    else
+    {
+      res.status(404).json({message: 'User not found'});
+    }
+});
+
+app.put('/users/:id', (req, res) => {
+  const {id}=req.params;
+  const {name, email}=req.body;
+  const user=users.find(u=>u.id==id);
+  if(user)
+  {
+    user.name = name;
+    user.email = email;
+    res.json(user);
+  }
+  else
+  {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
+
+
+app.delete('/users/:id', (req, res) => {
+    const {id}=req.params;
+    const index=users.findIndex(u=>u.id==id);
+    if(index!==-1)
+    {
+      users.splice(index,1);
+      res.status(204).send();
+    }
+    else
+    {
+      res.status(404).json({ message: 'User not found' });
+    }
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
 
 // Do not touch the code below this comment
 // **************************************************************
